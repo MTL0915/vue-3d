0603f632a51d803d48b527b3e30c47537dc99dcf
Matianle <18923236683@163.com>
2021-05-11 00:13:36 +0800

    model success

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,80 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  import * as Three from 'three'
+  import OrbitControls from 'three-orbitcontrols'
+  import {OBJLoader} from 'three-obj-mtl-loader'
+  export default {
+    name:"HelloWorld",
+    data () {
+      return {
+        // 相机
+        camera: null,
+        // 场景
+        scene: null,
+        // 渲染器对象
+        renderer: null,
+        // 材质对象
+        mesh: null,
+        controls:null,
+      }
+    },
+    methods: {
+      // 初始化
+      init () {
+        // 相机位置
+        let width=window.innerWidth;
+        let height=window.innerHeight;
+         // 创建场景
+        this.scene = new Three.Scene();
+        let k = width/height;
+        let s = 150;
+        this.camera = new Three.OrthographicCamera(-s*k,s*k,s,-s,1,1000);
+        this.camera.position.set(80, 100, 60);
+        this.camera.lookAt(new Three.Vector3(1, 80, 0));
+        this.scene.add(this.camera);
+        // 创建点光源
+        let light=new Three.DirectionalLight(0xffaaff);
+        light.position.set(80,100,50);
+        this.scene.add(light);
+        // 创建渲染器
+        this.renderer = new Three.WebGLRenderer({antialias: true})
+        this.renderer.setSize(width, height)
+        document.body.appendChild(this.renderer.domElement);
+        // 创建控件对象
+        this.controls=new OrbitControls(this.camera,this.renderer.domElement);
+        this.controls.addEventListener('change',this.animate);
+      },
+      // 创建obj模型
+      loadObj () {
+        let that=this;
+        let objloader=new OBJLoader();
+        objloader.load('/static/male02.obj',function(obj){
+          that.mesh=obj;
+          that.scene.add(that.mesh);
+          that.animate();
+        })
+      },
+      // 创建动画
+      animate () {
+        this.renderer.render(this.scene, this.camera)
+      },
+    },
+    mounted () {
+      this.init();
+      this.loadObj();
+    }
   }
-}
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  #container {
+    height: 800px;
+  }
+  #three{
+    height:800px;
+  }
 </style>
