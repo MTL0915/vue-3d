a5c81610f0786c2644d8c7454833174f6d2c6c31
Matianle0915 <18923236683@163.com>
2021-05-11 18:54:38 +0800

    add model

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <Cat msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cat from './components/Cat.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Cat
   }
 }
 </script>
